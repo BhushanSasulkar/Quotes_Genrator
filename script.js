9b183bc22d75e0f5b2fd3fd69d742736263fbce7
BhushanSasulkar <125477232+BhushanSasulkar@users.noreply.github.com>
2023-04-11 16:41:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText=document.getElementById("quote");
+const authorText=document.getElementById("author");
+const twitterBtn=document.getElementById("twitter");
+const newQuoteBtn=document.getElementById("new-quote");
+const loader = document.querySelector("span.loader");
+
+
+function loading(){
+    loader.classList.remove("hide");
+    quoteContainer.classList.add("hide");
+}
+
+function complete(){
+    loader.classList.add("hide");
+    quoteContainer.classList.remove("hide");
+}
+
+let apiQuotes=[];
+
+function newQuotes(){
+    const quote = apiQuotes[Math.floor(Math.random()*apiQuotes.length)];
+    quoteText.textContent=quote.text;
+    authorText.textContent=quote.author ? quote.author : "Unknown"; 
+    
+    if(quote.text.length>120){
+        quoteText.classList.add("long-quote");
+    }
+    else{
+        quoteText.classList.remove("long-quote");
+    }
+
+}
+
+
+async function getQuotes(){
+    loading();
+    const apiUrl="https://type.fit/api/quotes";
+    try {
+        const response=await fetch(apiUrl);
+        apiQuotes= await response.json();
+        newQuotes();
+        complete(); 
+    } catch (error) {
+        
+    }
+}
+
+function tweetQuote(){
+    const twitterUrl=`https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl,"_blank");
+}
+
+twitterBtn.addEventListener("click",tweetQuote);
+
+newQuoteBtn.addEventListener("click",getQuotes);
+
+getQuotes();
